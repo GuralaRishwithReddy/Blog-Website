56314b23d153cc099878e99811abaf7da8ce8e77
Your Name <rishigurala@gmail.com>
2023-07-28 22:06:06 +0530

    commited

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Logo from "../img/logo2.png"
+const Footer= () =>{
+    return <footer>
+        <img src={Logo} alt=""></img>
+        <span>Made with love from <b>Rishi</b>.</span>
+    </footer>
+}
+
+export default Footer
